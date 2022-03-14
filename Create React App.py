71d686c33f10eb8a14edd2934b290a78c94f9908
some.one.who.codez <agmadaasa@gmail.com>
2022-03-14 17:12:30 +0000

    Updated Main File �🚀

--- Create React App.py
+++ Create React App.py
@@ -1,14 +1,23 @@
-import os
+import os # needed to run terminals
 
-appName = input("What is your React app name ")
-cmd = "npx create-react-app " + appName
-cd = "cd " + appName
-start = "npm start"
-vsc = "code ."
+# get details of app
+appName = input("What is your React app name? ")
+directory = input("Which directory do you wish to place it in? ") # eg 'C:\Users\YOUR_USERNAME\Documents\react'
 
-os.system('cmd /c ' + cmd)
-os.system('cmd /c ' + cd)
-os.chdir(appName)
+directoryOfApp = directory + '\\' + appName # eg 'C:\Users\YOUR_USERNAME\Documents\react\YOUR_APP_NAME'
+createApp = "npx create-react-app " + appName # creates app
+start = "npm start" # start react app
+vsc = "code ." # opens VSX
+
+os.chdir(directory)
+
+# create app
+os.system('cmd /c ' + createApp)
+
+# navigate to app dir
+os.chdir(directoryOfApp)
+
+# open VSC and start react app
 os.system('cmd /c ' + vsc)
 os.system('cmd /c ' + start)
 
