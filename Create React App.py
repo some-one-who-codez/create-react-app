753bc5f043676976f6941cafc8418c1c5b551759
some.one.who.codez <agmadaasa@gmail.com>
2024-01-06 18:14:50 +0000

    Typescript support

--- Create React App.py
+++ Create React App.py
@@ -7,16 +7,25 @@ directory = input("Which directory do you wish to place it in? ") # eg 'C:\Users
 directoryOfApp = directory + '\\' + appName # eg 'C:\Users\YOUR_USERNAME\Documents\react\YOUR_APP_NAME'
 use_vite = str(input("Do you want to use vite? (y/N) ")).lower()
 use_tailwind = str(input("Do you want to use tailwind? (y/N) ")).lower()
+use_typescript = str(input("Do you want to use typescript? (y/N) ")).lower()
 
 createApp = ""
 start = ""
 
 if use_vite == "yes" or use_vite == 'y':
-    createApp = "npm create vite@latest " + appName + " -- --template react"  # creates app
-    start = "npm run dev" # start react app
+    if(use_typescript == "yes" or use_typescript == 'y'):
+       createApp = "npm create vite@latest " + appName + " -- --template react-ts"  # creates app
+       start = "npm run dev" # start react app 
+    else:
+        createApp = "npm create vite@latest " + appName + " -- --template react"  # creates app
+        start = "npm run dev" # start react app
 else:
-    createApp = "npx create-react-app " + appName # creates app
-    start = "npm start" # start react app
+    if(use_typescript == "yes" or use_typescript == 'y'):
+        reateApp = "npx create-react-app " + appName + " --template typescript" # creates app
+        start = "npm start" # start react app
+    else: 
+        createApp = "npx create-react-app " + appName # creates app
+        start = "npm start" # start react app
 
 vsc = "code ." # opens VSC
 
